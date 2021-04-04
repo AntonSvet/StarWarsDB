fa9ebfafc688b6dac900096386dcbe63b151d124
Anton <waytofall@inbox.ru>
2021-04-04 15:50:04 +0300

    'starDB'

---
+++ src/components/pages/index.js
@@ -0,0 +1,13 @@
+import PeoplePage from './people-page';
+import PlanetsPage from './planets-page';
+import StarshipsPage from './starships-page';
+import LoginPage from "./loginPage";
+import SecretPage from "./secret-page";
+
+export {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+    LoginPage,
+    SecretPage
+};
